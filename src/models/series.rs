70fcf7f6f4ba451721733762a9c9b9c9be4c19c4
Naiker <yinho999@gmail.com>
2024-01-16 07:22:23 +0000

    Updating series

--- src/models/series.rs
+++ src/models/series.rs
@@ -1,14 +1,16 @@
 use std::result;
+use loco_rs::model::ModelError;
 use sea_orm::entity::prelude::*;
 use sea_orm::{Set, TransactionTrait};
 use serde::{Deserialize, Serialize};
-use crate::models::_entities::series;
+use crate::models::_entities::{posts, series, users};
 use crate::models::ModelsError;
 use super::_entities::series::ActiveModel;
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct SeriesParams {
     pub title: String,
     pub description: String,
+    pub posts: Vec<i32>,
 }
 
 impl SeriesParams {
@@ -33,9 +35,23 @@ impl super::_entities::series::Model {
     pub async fn create(
         db: &DatabaseConnection,
         params: &SeriesParams,
-
+        pid: Uuid,
     ) -> result::Result<Self, ModelsError> {
         let txn = db.begin().await?;
+        // Get the user
+        let user = users::Entity::find()
+            .filter(users::Column::Pid.eq(pid))
+            .one(&txn)
+            .await?;
+        let user = match user {
+            Some(user) => user,
+            None => return Err(ModelError::EntityNotFound.into()),
+        };
+        // find all posts that is within the posts vec
+        let posts = posts::Entity::find()
+            .filter(posts::Column::Id.is_in(params.posts.clone()))
+            .all(&txn)
+            .await?;
         let item = params.create();
         let item = item.insert(&txn).await?;
         txn.commit().await?;
